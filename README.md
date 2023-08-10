f18322dc29446822767771873534d8bfb31186ae
Rajeshwar <rajeshwarrao055@gmail.com>
2023-08-10 22:24:19 +0530

    Rajeshwar | Minor fixes

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ MINIO_ROOT_USER=admin MINIO_ROOT_PASSWORD=password minio server /Users/rajeshwar
 
 * Spark installation 
   * https://spark.apache.org/downloads.html - Download zip from here 
-  * tar xzvf apache-maven-3.9.2-bin.tar.gz - extract to directory
+  * tar xzvf spark-3.3.2-bin-hadoop3.tgz - extract to directory
   * sudo mv spark-3.3.2-bin-hadoop3 /usr/local/spark - move to /usr/local/spark
 
 * Airflow Installation 
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def read_from_spark():
         .appName("READ_FROM_SPARK") \
         .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-        .config("spark.hadoop.fs.s3a.access.key", "iEwFAtT86AoLTezczynQ") \
-        .config("spark.hadoop.fs.s3a.secret.key", "oy2Gwn7324Cw3JgHvrNHEv0HwYFjEV7Lrr3dgIte") \
+        .config("spark.hadoop.fs.s3a.access.key", "AOAbxzELSriTadwoIwBN") \
+        .config("spark.hadoop.fs.s3a.secret.key", "LnTM96i1EImstHWZgRn5F4qofJsO1c5SFT4r1unK") \
         .config("spark.hadoop.fs.s3a.path.style.access", "true") \
         .getOrCreate()
-    df = spark.read.csv("s3a://test-bucket/impacted_customers_22_jan_2023.csv")
+    df = spark.read.csv("s3a://test-bucket/some_file.csv")
     df.show()
 
 
